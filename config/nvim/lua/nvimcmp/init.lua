a0466607b8d13f15d98fbf2b7ad28c0cf3801ac0
Gabe Black <gabe@gabe.black>
2023-07-12 09:53:41 -0500

    add ctrl j/k to select nvim cmp suggestions

--- config/nvim/lua/nvimcmp/init.lua
+++ config/nvim/lua/nvimcmp/init.lua
@@ -18,6 +18,8 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
